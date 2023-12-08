186aa45ecb0893bf18b29dfdcc655a310a9cc215
Sơn Nguyễn <owaf.fakku@hotmail.com>
2023-12-08 11:45:16 +0700

    Add project files.

---
+++ Print Avatar ID/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Print_Avatar_ID
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(139, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Kết quả";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Chọn ảnh";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(226, 45);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Ghép ảnh thẻ 4x6";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Print Avatar ID/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+
+namespace Print_Avatar_ID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sourceImagePath = ChooseImageFile();
+
+            if (string.IsNullOrEmpty(sourceImagePath))
+            {
+                Console.WriteLine("Không có ảnh nào được chọn.");
+                return;
+            }
+            string outputImagePath = "Ok.jpg"; // Đường dẫn lưu ảnh kết quả
+
+            // Resize ảnh nhỏ về kích thước 4x6 cm (472x708 pixels)
+            Size smallImageSize = new Size(472, 708);
+
+            // Kích thước tờ A4 (2480x3508 pixels)
+            Size a4Size = new Size(2480, 3508);
+
+            // Khoảng cách giữa các ảnh
+            int spacing = 1; // Thay đổi giá trị này để thay đổi khoảng cách
+
+            using (Image sourceImage = Image.FromFile(sourceImagePath))
+            using (Bitmap a4Image = new Bitmap(a4Size.Width, a4Size.Height))
+            using (Graphics g = Graphics.FromImage(a4Image))
+            {
+                g.Clear(Color.White); // Nền trắng
+
+                for (int y = 0; y + smallImageSize.Height <= a4Size.Height; y += smallImageSize.Height + spacing)
+                {
+                    for (int x = 0; x + smallImageSize.Width <= a4Size.Width; x += smallImageSize.Width + spacing)
+                    {
+                        g.DrawImage(sourceImage, new Rectangle(x+40, y+40, smallImageSize.Width, smallImageSize.Height));
+                    }
+                }
+
+                a4Image.Save(outputImagePath); // Lưu ảnh
+            }
+
+            Console.WriteLine("Ảnh đã được ghép và lưu thành công!");
+        }
+
+        static string ChooseImageFile()
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Title = "Chọn ảnh";
+                dialog.Filter = "Image files (*.jpg;*.jpeg;*.png)|*.jpg;*.jpeg;*.png";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    return dialog.FileName;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenDirectory(AppDomain.CurrentDomain.BaseDirectory);
+        }
+        static void OpenDirectory(string path)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = path,
+                    UseShellExecute = true,
+                    Verb = "open"
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Không thể mở thư mục: {ex.Message}");
+            }
+        }
+    }
+}
